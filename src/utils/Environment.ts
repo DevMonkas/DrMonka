fcad9cde3af55f5ef3bd829b34a4f10e9cda847b
flyingninja97 <ananychitranshi0701@gmail.com>
2021-10-15 12:33:37 +0530

    base url changed

--- src/utils/Environment.ts
+++ src/utils/Environment.ts
@@ -2,7 +2,7 @@ var _Environments = {
   production: {BASE_URL: '', API_KEY: ''},
   staging: {BASE_URL: '', API_KEY: ''},
   development: {
-    BASE_URL: 'https://9052-2405-201-8000-d0d3-552e-137-19f7-63e7.ngrok.io',
+    BASE_URL: 'https://qivihealth-backend.herokuapp.com/',
     API_KEY: '',
   },
 };
