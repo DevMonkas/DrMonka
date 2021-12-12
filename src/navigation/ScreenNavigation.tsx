48fdfc2ee6b3133391a924d218ee6347145a00c0
flyingninja97 <ananychitranshi0701@gmail.com>
2021-12-12 14:12:59 +0530

    chat fetch api and filter

--- src/navigation/ScreenNavigation.tsx
+++ src/navigation/ScreenNavigation.tsx
@@ -31,11 +31,14 @@ import Feather from 'react-native-vector-icons/Feather';
 import VideoCall from '../pages/VideoCall/VideoCall';
 import {AuthContext} from '../shared/AuthProvider';
 import {VideoCallContext} from '../shared/VideoCallProvider';
+import {GlobaluserObj, assignValue} from '../shared/Globals';
 const Stack = createStackNavigator();
 
 export default function ScreenNavigation({viewedOnboarding}: any) {
   const soc = useContext(SocketContext);
   const [user, setUser] = useContext(AuthContext);
+  assignValue(user);
+
   const [messageObj, setMessageObj] = useContext(MessageContext);
   const {initialize} = useContext(VideoCallContext);
   useEffect(() => {
--- src/utils/Environment.ts
+++ src/utils/Environment.ts
@@ -2,7 +2,7 @@ var _Environments = {
   production: {BASE_URL: '', API_KEY: ''},
   staging: {BASE_URL: '', API_KEY: ''},
   development: {
-    BASE_URL: 'https://qivihealth-backend.herokuapp.com', //"https://topastro-backend.herokuapp.com",
+    BASE_URL: 'http://localhost:8000', //"https://qivihealth-backend.herokuapp.com",
     // BASE_URL: 'https://0b33-2405-201-8000-d122-b9ed-5834-7a8d-eb8.ngrok.io',
     API_KEY: '',
   },
